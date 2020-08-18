1f83683a34d8f4f42b6370773d5a8fa3e17e3199
shenjl <1109925966@qq.com>
2020-08-18 09:31:27 +0800

    Update hello.sh
    replace server ip

--- hello.sh
+++ hello.sh
@@ -2,8 +2,8 @@ echo "hello world" > /root/hello.txt
 
 yum install zabbix-agent
 
-sed -i 's/Server=127.0.0.1/Server=19.52.68.169/g' /etc/zabbix/zabbix_agentd.conf
-sed -i 's/ServerActive=127.0.0.1/ServerActive=19.52.68.169/g' /etc/zabbix/zabbix_agentd.conf
+sed -i 's/^Server=.*$/Server=19.52.68.169/g' /etc/zabbix/zabbix_agentd.conf
+sed -i 's/^ServerActive=.*$/ServerActive=19.52.68.169/g' /etc/zabbix/zabbix_agentd.conf
 
 
 grep "Server" /etc/zabbix/zabbix_agentd.conf | grep -v '^#'
