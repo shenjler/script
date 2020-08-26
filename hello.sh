80d4e6633b9b43018901ceb7f1f9e131c6fc3b73
shenjl <1109925966@qq.com>
2020-08-26 17:53:41 +0800

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,12 +1,4 @@
 echo "hello world" > /root/hello.txt
 
-yum install zabbix-agent
-
-sed -i 's/^Server=.*$/Server=19.52.68.169/g' /etc/zabbix/zabbix_agentd.conf
-sed -i 's/^ServerActive=.*$/ServerActive=19.52.68.169/g' /etc/zabbix/zabbix_agentd.conf
-
-
-grep "Server" /etc/zabbix/zabbix_agentd.conf | grep -v '^#'
-
-
-service zabbix-agent restart
+echo $1
+echo $2
