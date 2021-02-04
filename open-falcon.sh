dd4a978a7de6bc8a969a771f96f774effec2d776
shenjl <1109925966@qq.com>
2021-02-04 17:05:14 +0800

    Create open-falcon.sh

---
+++ open-falcon.sh
@@ -0,0 +1,8 @@
+cd /tmp/ && git clone https://github.com/open-falcon/falcon-plus.git 
+cd /tmp/falcon-plus/scripts/mysql/db_schema/
+mysql -h 127.0.0.1 -u root -pPccw@123456 < 1_uic-db-schema.sql
+mysql -h 127.0.0.1 -u root -pPccw@123456 < 2_portal-db-schema.sql
+mysql -h 127.0.0.1 -u root -pPccw@123456 < 3_dashboard-db-schema.sql
+mysql -h 127.0.0.1 -u root -pPccw@123456 < 4_graph-db-schema.sql
+mysql -h 127.0.0.1 -u root -pPccw@123456 < 5_alarms-db-schema.sql
+rm -rf /tmp/falcon-plus/
