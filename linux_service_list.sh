14e79a6e6ba44bdee338c5aa0eb993b4cf601c37
shenjl <shenjl@qq.com>
2021-04-07 14:39:01 +0800

    ‘list linux service update

--- linux_service_list.sh
+++ linux_service_list.sh
@@ -2,12 +2,14 @@
 # 查看开机启动项
 systemctl list-unit-files | grep enable
 
+
+
 # springcloud微服务 
 systemctl list-unit-files | grep -nE '(gateway|^common|workflow|customer|automation|^cmdb|stock|knowledge|report|^log|devops|^notify|^job|gis|^probe)'
 
 systemctl list-unit-files | grep enable | grep -nE '(gateway|^common|workflow|customer|automation|^cmdb|stock|knowledge|report|^log|devops|^notify|^job|gis|^probe)'
 
-ll /usr/lib/systemd/system/ | grep -nE '(gateway|^common|workflow|customer|automation|^cmdb|stock|knowledge|report|^log|devops|^notify|^job|gis|^probe)'
+ls -l /usr/lib/systemd/system/ | grep -nE '(gateway|common|workflow|customer|automation|cmdb|stock|knowledge|report|log.service|devops|notify|job.service|gis|probe)'
 
 
-
+ls -l /data/eoms/*.jar
